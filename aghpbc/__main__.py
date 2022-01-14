066c7dd70dac04d63d9b9f3e818be452f2309e2e
Sourajyoti Basak <wiz28@protonmail.com>
2022-01-14 20:48:22 +0530

    feat: initial script

---
+++ aghpbc/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import click
+
+
+@click.command()
+def main() -> None:
+    """A CLI interface for displaying anime girls holding programming books."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
